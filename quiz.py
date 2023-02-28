a804fed7616e8c12f872b2e0ecdfa872f9811cb3
kyaakpa <100368545+kyaakpa@users.noreply.github.com>
2023-02-28 07:03:18 -0500

    Add quiz.py

---
+++ quiz.py
@@ -0,0 +1,34 @@
+print("Welcome to quiz!")
+
+playing = input("Would you like to play? ")
+
+if playing.lower().strip() != "yes":
+    quit()
+
+print("Here we go!!!")
+score = 0
+
+answer = input('What does GPU stand for? ')
+if answer.lower().strip() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print('Incorrect!')
+
+answer = input('What does RAM stand for? ')
+if answer.lower().strip() == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print('Incorrect!')
+
+answer = input('What does PSU stand for? ')
+if answer.lower().strip() == "power supply unit":
+    print('Correct!')
+    score += 1
+
+else:
+    print('Incorrect!')
+
+print("Total score: " + str(score))
+print("total score: " + str((score / 3) * 100) + "%")
